705caaae085f2a9cbf521c24d880e9bb059ddbba
Aspen Chidester <chi21051@byui.edu>
2022-09-27 16:13:03 -0600

    changes

--- lesson02/js/design-principles.js
+++ lesson02/js/design-principles.js
@@ -1,3 +1,8 @@
 const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
 document.getElementById("lastupdated").textContent = document.lastModified;
+//Better way to do this...
+// window.addEventListener('load', () => {
+//     const today = new Date();
+//     document.getElementById('currrentyear').textContent = today.getFullYear();
+// });
