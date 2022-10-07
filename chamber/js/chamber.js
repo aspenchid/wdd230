8e600e32a864fe1404e645f9aaf61d4a6592e954
Aspen Chidester <chi21051@byui.edu>
2022-10-07 13:09:23 -0600

    entire website

---
+++ chamber/js/chamber.js
@@ -0,0 +1,19 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+function toggleMenu(){
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+const today = new Date();
+document.querySelector("footer div span").textContent = today.getFullYear();
+
+document.getElementById("lastupdated").textContent = document.lastModified;
