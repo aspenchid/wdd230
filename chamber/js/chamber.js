652c24cfeb73253d5b8ff44c7ab8331df26b347f
Aspen Chidester <chi21051@byui.edu>
2022-10-20 15:41:51 -0600

    Java and Border fixing

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -17,8 +17,30 @@ const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
 
 // make this actually == 1, 2
-if (today.getDay() == 1, 2){
+if (today.getDay() == 1){
     document.querySelector("header p").style.display="block";
 }
+if (today.getDay() == 2){
+    document.querySelector("header p").style.display="block";
+}
+
+function setwindchill(temp, windspeed){
+    let tempobj = document.querySelector("#temperature");
+    let windspeedobj = document.querySelector("#windspeed");
+    let windchillobj = document.querySelector("#windchill");
+
+    let windchillmsg = "N/A";
+
+    if (temp <= 50 && windspeed > 3){
+        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+        windchillmsg = `${chill}&deg; F`
+    }
+
+    tempobj.textContent = temp;
+    windspeedobj.textContent = windspeed;
+    windchillobj.innerHTML = windchillmsg;
+}
+setwindchill(49, 10)
+
 
 document.getElementById("lastupdated").textContent = document.lastModified;
